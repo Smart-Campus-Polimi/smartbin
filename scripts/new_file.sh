e7194c840d297d5810f39b08b515e42e6dd0fb66
edoardesd <edoarded@gmail.com>
2019-02-07 21:13:00 +0100

    no fps in webcam

--- scripts/new_file.sh
+++ scripts/new_file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-inotifywait -m webcam -e create -e moved_to |
+inotifywait -m ~/pictures -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         
-        scp $path/$file drosdesd@192.168.43.167:/Users/drosdesd/rasp/	
+        scp $path/$file drosdesd@192.168.1.245:/Users/drosdesd/rasp/	
 
     done
 
