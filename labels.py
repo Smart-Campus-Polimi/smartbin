b0c79ddeea7d895e8b28e5b6698c7bdbc0377ed2
edoardesd <edoardesd@gmail.com>
2019-02-07 11:53:16 +0000

    add reko on real bin

---
+++ labels.py
@@ -0,0 +1,6 @@
+plastic = ["Bottle", "Water Bottle", "Mineral Water", "Cup", "Drink", "Coffee Cup", "Plastic", "Plastic Bag", "Plastic Wrap","Water Jug", "Pop Bottle", "Beverage", "Pop Bottle"]
+aluminium = ["Can", "Aluminium", "Tin", "Soda", "Drink", "Coke", "Canned Goods", "Beer", "Foil", "Insulation", "Beverage", "Keg"]
+paper = ["Paper", "Poster", "Diagram", "White Board", "Brochure", "Menu", "Origami", "Cardboard", "Carton", "Book", "Page", "Letter", "Scroll", "Text", "Paper Towel", "Document", "License", "Driving License", "Alphabet", "Diploma", "Id Cards", "Passport", "Word", "Envelope", "Newspaper"]
+glass = ["Glass", "Beer", "Lager", "Beer Bottle", "Pop Bottle", "Wine Glass", "Drink", "Wine", "Green", "Liquor", "Jar", "Beer Glass", "Goblet", "Beverage"]
+
+drop = ['Cloghing', 'Helmet', 'Apparel', 'Light Fixture','Handrail','Banister', 'Lampshade', 'Lamp', 'Lighting','Staircase', 'Architecture', 'Building', 'Sink Faucet', 'Mirror', 'Arch', 'Arched', 'Hoop']
