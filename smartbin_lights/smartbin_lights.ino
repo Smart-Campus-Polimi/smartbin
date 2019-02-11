a21f1eea4009357775dcb19b4021924c19a53ba0
edoardesd <edoardesd@gmail.com>
2019-02-11 08:45:44 +0100

    new arduino message type & breathe effect

--- smartbin_lights/smartbin_lights.ino
+++ smartbin_lights/smartbin_lights.ino
@@ -6,7 +6,12 @@
 
 Adafruit_NeoPixel led_door = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int door_status = 0;
+int msg;
+int msgLength;
+char payload[10];
+char msgType;
+bool inMsg = false;
+bool endOfMsg = false;
 
 void setup()
 {
@@ -22,32 +27,97 @@ void setup()
 
 void loop()
 {
-  Serial.println(door_status);
-  if (Serial.available()>0) {
-    door_status=Serial.read();
-    
-    switch(door_status){
-      case 65:
-          offCameraLight();
-          break;
-
-      case 66:
-          onCameraLight();
-          break;
-
-      case 67:
-          blinkCameraLight();
-          break;
-          
-      default:
-          offCameraLight();
-          break;
-    }
-    
-  }
   
+  if (Serial.available()>0) {
+    msg=Serial.read();
+    //Serial.println(msg);
+    
+    if(msg == 35){
+      inMsg = true;
+      msgLength = -1;
+   
+    }else{
+    
+      if(msg == 33){
+        inMsg = false;
+        endOfMsg = true;
+      }
+
+      if(inMsg){
+        if(msgLength < 0){
+          msgType = (char)msg;
+        
+        }else{
+          payload[msgLength] = (char)msg;
+          
+        }
+        msgLength++;
+      }
+    }
+
+    if(endOfMsg){
+      payload[msgLength] = '\0';
+      endOfMsg = false;
+      switchData(payload);
+      
+    }
+  }
+
 }
 
+void switchData(char p[]){
+  
+  int n = atoi(p);
+  Serial.println(msgType);
+  switch(msgType){
+    case 'P':
+      Serial.println("Plastic to ");
+      Serial.println(n);
+      break;
+    case 'U':
+      Serial.println("Unsorted to ");
+      Serial.println(n);
+      break;
+    case 'C':
+      Serial.println("Paper (C) to ");
+      Serial.println(n);
+      break;
+    case 'G':
+      Serial.println("Glass to ");
+      Serial.println(n);
+      break;
+    case 'D':
+      Serial.println("Door to ");
+      Serial.println(n);
+      switchDoor(n);
+      break;
+    case 'R':
+      Serial.println("Ring to ");
+      Serial.println(n);
+      break;
+    case 'S':
+      Serial.println("Sportello to ");
+      Serial.println(n);
+      break;
+     
+  }
+}
+
+void switchDoor(char my_type){
+  Serial.println("type : ");
+  Serial.print(my_type);
+  switch(my_type){
+    case 0:
+      offCameraLight();
+      break;
+    case 1:
+      onCameraLight();
+      break;
+    case 2:
+      blinkCameraLight();
+      break;
+  }
+}
 void onCameraLight(){
   //Serial.println("turn on the door led");
   for(int i=0;i<NUMPIXELS;i++){
@@ -65,10 +135,32 @@ void offCameraLight(){
 }
 
 void blinkCameraLight(){
+  
+  /*
   onCameraLight();
   delay(500);
   offCameraLight();
   delay(500);
+  */
+  float frequency = 0.008;
+  float maximumBrightness = 255;
+  
+  for (int i = 0; i < 2000; i++) {
+    Serial.println(frequency*i);
+    float intensity = maximumBrightness/2.0 * (1.0 + sin(frequency * i));
+    if(intensity == 10){
+       Serial.println("-------------_");
+      Serial.println(i);
+      delay(10);
+     
+    }
+    led_door.setBrightness(intensity);
+    for (int ledNumber=0; ledNumber<NUMPIXELS; ledNumber++) {
+      led_door.setPixelColor(ledNumber, 0, 0, 255);
+    }
+
+    led_door.show();
+  }
 }
 
 
