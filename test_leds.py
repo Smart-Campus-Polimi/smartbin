ecbdf015f76309b4d765c6286ae7a8e578732d7b
edoardesd <edoarded@gmail.com>
2020-02-06 14:32:39 +0100

    ToF binaries

---
+++ test_leds.py
@@ -0,0 +1,13 @@
+import serial
+import SerialHandler
+
+print("### Serial controller starts!")
+serialComm = SerialHandler.SerialHandler()
+ser = serialComm.getSerialPort()
+
+while True:
+    keyInput = input("Type a char").upper()
+    print("You typed {}".format(keyInput))
+    led_command = str.encode('#{}R1!'.format(keyInput))
+    ser.write(led_command)
+
