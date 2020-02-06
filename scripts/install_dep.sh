142a704732f92a4512afaa276651c165b4088594
edoardesd <edoarded@gmail.com>
2020-02-06 11:19:23 +0100

    add dependencies info

---
+++ scripts/install_dep.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev \
+    libsmpeg-dev libportmidi-dev libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev mercurial
+
+pip install -r ../requirements.txt
